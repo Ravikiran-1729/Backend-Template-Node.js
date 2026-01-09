3aa8f596bc3fdc8de8a4bdb0b8e99dc56dd066b2
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2026-01-10 01:56:39 +0530

    Updated public

--- public/JS/main.js
+++ public/JS/main.js
@@ -2,6 +2,7 @@ const btn = document.querySelector('button');
 
 btn.addEventListener('click', (event)=>{
     event.stopPropagation();
+    
     console.log("Button Clicked");
     alert("Congratulations!! Template Setup is done.")
 });
