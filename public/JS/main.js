cba136670347a73af2b8b5b1ae019b0bc03ac9e3
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2026-01-09 21:25:34 +0530

    Backend layout

---
+++ public/JS/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', (event)=>{
+    event.stopPropagation();
+    console.log("Button Clicked");
+    alert("Congratulations!! Template Setup is done.")
+});
